3616f9333c0ae15c058f0ced9b28487e3387ec36
rezaerbe <rezarb88@gmail.com>
2022-10-28 13:41:53 +0700

    WIP: Bottom sheet component

---
+++ feature/component/src/main/java/com/erbeandroid/petfinder/feature/component/list/component/BottomSheetComponent.kt
@@ -0,0 +1,21 @@
+package com.erbeandroid.petfinder.feature.component.list.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.erbeandroid.petfinder.feature.component.databinding.ComponentBottomSheetBinding
+
+class BottomSheetComponent @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding =
+        ComponentBottomSheetBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+
+    }
+}
